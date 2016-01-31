27d0ec774e16bb5f6e88435208cdb118d54148ab
mammenx <mammenx@gmail.com>
2016-01-31 18:14:47 +0530

    .

---
+++ wxp/dgn/rtl/l2/peg_l2_params.sv
@@ -0,0 +1,130 @@
+/*
+ --------------------------------------------------------------------------
+   Pegasus - Copyright (C) 2012 Gregory Matthew James.
+
+   This file is part of Pegasus.
+
+   Pegasus is free; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   Pegasus is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+*/
+
+/*
+ --------------------------------------------------------------------------
+ -- Project Code      : pegasus
+ -- Package Name      : peg_l2_params
+ -- Author            : mammenx
+ -- Description       : This file contains the different parameters
+                        and definitions used in L2 block.
+ --------------------------------------------------------------------------
+*/
+
+  parameter PREAMBLE_VALUE      = 8'b01010101;
+  parameter SFD_VALUE           = 8'b11010101;
+  parameter VLAN_TYPE_VALUE     = 16'h8100;
+  parameter CTRL_TYPE_VALUE     = 16'h8808;
+  parameter PAUSE_CTRL_OPCODE   = 16'h0001;
+  parameter GLBL_MULTICAST_ADDR = 48'h01_80_C2_00_00_01;
+
+  parameter RS_TYPE           = "RMII";
+
+  //Byte offsets for different MAC fields
+  parameter MAC_PREAMBLE_OFFSET   = 0;
+  parameter MAC_SFD_OFFSET        = 7;
+  parameter MAC_DA_OFFSET         = 8;
+  parameter MAC_SA_OFFSET         = 14;
+  parameter MAC_LEN_TYPE_OFFSET   = 20;
+  parameter MAC_CTRL_OPCODE_OFFSET= 22;
+  parameter MAC_DATA_OFFSET       = 22;
+  parameter MAC_PAUSE_TIME_OFFSET = 24;
+  parameter MAC_VLAN_DATA_OFFSET  = 26;
+
+  //Parameters for indexing the field buffer
+  parameter MAC_FIDX_DADDR        = 0;
+  parameter MAC_FIDX_SADDR        = 1;
+  parameter MAC_FIDX_LEN_TYPE     = 2;
+  parameter MAC_FIDX_VLAN_TAG     = 3;
+  parameter MAC_FIDX_CTRL_OPCODE  = 4;
+  parameter MAC_FIDX_PAUSE_TIME   = 5;
+  parameter MAC_FIDX_FCS          = 6;
+
+  //Function to calculate CRC
+  function [31:0] nextCRC32_D8;
+
+    input [7:0] Data;
+    input [31:0] crc;
+    reg [7:0] d;
+    reg [31:0] c;
+    reg [31:0] newcrc;
+  begin
+    d = Data;
+    c = crc;
+
+    newcrc[0] = d[6] ^ d[0] ^ c[24] ^ c[30];
+    newcrc[1] = d[7] ^ d[6] ^ d[1] ^ d[0] ^ c[24] ^ c[25] ^ c[30] ^ c[31];
+    newcrc[2] = d[7] ^ d[6] ^ d[2] ^ d[1] ^ d[0] ^ c[24] ^ c[25] ^ c[26] ^ c[30] ^ c[31];
+    newcrc[3] = d[7] ^ d[3] ^ d[2] ^ d[1] ^ c[25] ^ c[26] ^ c[27] ^ c[31];
+    newcrc[4] = d[6] ^ d[4] ^ d[3] ^ d[2] ^ d[0] ^ c[24] ^ c[26] ^ c[27] ^ c[28] ^ c[30];
+    newcrc[5] = d[7] ^ d[6] ^ d[5] ^ d[4] ^ d[3] ^ d[1] ^ d[0] ^ c[24] ^ c[25] ^ c[27] ^ c[28] ^ c[29] ^ c[30] ^ c[31];
+    newcrc[6] = d[7] ^ d[6] ^ d[5] ^ d[4] ^ d[2] ^ d[1] ^ c[25] ^ c[26] ^ c[28] ^ c[29] ^ c[30] ^ c[31];
+    newcrc[7] = d[7] ^ d[5] ^ d[3] ^ d[2] ^ d[0] ^ c[24] ^ c[26] ^ c[27] ^ c[29] ^ c[31];
+    newcrc[8] = d[4] ^ d[3] ^ d[1] ^ d[0] ^ c[0] ^ c[24] ^ c[25] ^ c[27] ^ c[28];
+    newcrc[9] = d[5] ^ d[4] ^ d[2] ^ d[1] ^ c[1] ^ c[25] ^ c[26] ^ c[28] ^ c[29];
+    newcrc[10] = d[5] ^ d[3] ^ d[2] ^ d[0] ^ c[2] ^ c[24] ^ c[26] ^ c[27] ^ c[29];
+    newcrc[11] = d[4] ^ d[3] ^ d[1] ^ d[0] ^ c[3] ^ c[24] ^ c[25] ^ c[27] ^ c[28];
+    newcrc[12] = d[6] ^ d[5] ^ d[4] ^ d[2] ^ d[1] ^ d[0] ^ c[4] ^ c[24] ^ c[25] ^ c[26] ^ c[28] ^ c[29] ^ c[30];
+    newcrc[13] = d[7] ^ d[6] ^ d[5] ^ d[3] ^ d[2] ^ d[1] ^ c[5] ^ c[25] ^ c[26] ^ c[27] ^ c[29] ^ c[30] ^ c[31];
+    newcrc[14] = d[7] ^ d[6] ^ d[4] ^ d[3] ^ d[2] ^ c[6] ^ c[26] ^ c[27] ^ c[28] ^ c[30] ^ c[31];
+    newcrc[15] = d[7] ^ d[5] ^ d[4] ^ d[3] ^ c[7] ^ c[27] ^ c[28] ^ c[29] ^ c[31];
+    newcrc[16] = d[5] ^ d[4] ^ d[0] ^ c[8] ^ c[24] ^ c[28] ^ c[29];
+    newcrc[17] = d[6] ^ d[5] ^ d[1] ^ c[9] ^ c[25] ^ c[29] ^ c[30];
+    newcrc[18] = d[7] ^ d[6] ^ d[2] ^ c[10] ^ c[26] ^ c[30] ^ c[31];
+    newcrc[19] = d[7] ^ d[3] ^ c[11] ^ c[27] ^ c[31];
+    newcrc[20] = d[4] ^ c[12] ^ c[28];
+    newcrc[21] = d[5] ^ c[13] ^ c[29];
+    newcrc[22] = d[0] ^ c[14] ^ c[24];
+    newcrc[23] = d[6] ^ d[1] ^ d[0] ^ c[15] ^ c[24] ^ c[25] ^ c[30];
+    newcrc[24] = d[7] ^ d[2] ^ d[1] ^ c[16] ^ c[25] ^ c[26] ^ c[31];
+    newcrc[25] = d[3] ^ d[2] ^ c[17] ^ c[26] ^ c[27];
+    newcrc[26] = d[6] ^ d[4] ^ d[3] ^ d[0] ^ c[18] ^ c[24] ^ c[27] ^ c[28] ^ c[30];
+    newcrc[27] = d[7] ^ d[5] ^ d[4] ^ d[1] ^ c[19] ^ c[25] ^ c[28] ^ c[29] ^ c[31];
+    newcrc[28] = d[6] ^ d[5] ^ d[2] ^ c[20] ^ c[26] ^ c[29] ^ c[30];
+    newcrc[29] = d[7] ^ d[6] ^ d[3] ^ c[21] ^ c[27] ^ c[30] ^ c[31];
+    newcrc[30] = d[7] ^ d[4] ^ c[22] ^ c[28] ^ c[31];
+    newcrc[31] = d[5] ^ c[23] ^ c[29];
+    nextCRC32_D8 = newcrc;
+  end
+  endfunction
+
+/*
+ --------------------------------------------------------------------------
+
+ -- <Header>
+ 
+
+ -- <Log>
+
+[28-07-2014  12:12:47 PM][mammenx] Added Pause Frame support
+
+[02-07-2014  12:53:29 AM][mammenx] Added misc MAC parameters & CRC function
+
+[28-06-2014  04:43:57 PM][mammenx] Removed System Verilog
+
+[24-06-2014  08:01:48 PM][mammenx] Added CRC function
+
+[18-06-2014  08:39:55 PM][mammenx] Added VLAN Tag & MAC Header structure types
+
+[18-06-2014  07:27:24 PM][mammenx] Initial Commit
+
+ --------------------------------------------------------------------------
+*/
