1103eade21e45b23ee90c6cae5a0cf741c20a048
mammenx <mammenx@gmail.com>
2016-01-31 16:27:46 +0530

    Initial Commit

---
+++ wxp/verif/tb/seq/peg_l2_pkt_seq.sv
@@ -0,0 +1,90 @@
+/*
+ --------------------------------------------------------------------------
+   Pegasus - Copyright (C) 2012 Gregory Matthew James.
+
+   This file is part of Pegasus.
+
+   Pegasus is free; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   Pegasus is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+*/
+
+/*
+ --------------------------------------------------------------------------
+ -- Project Code      : pegasus
+ -- Sequence Name     : peg_l2_pkt_seq
+ -- Author            : mammenx
+ -- Function          : This sequence generates a l2 packet for transmission
+ --------------------------------------------------------------------------
+*/
+
+`ifndef __PEG_L2_PKT_SEQ
+`define __PEG_L2_PKT_SEQ
+
+  class peg_l2_pkt_seq   #(
+                             type  PKT_TYPE  =  peg_l2_pkt,
+                             type  SEQR_TYPE =  peg_rmii_rx_seqr#(PKT_TYPE)
+                          ) extends ovm_sequence  #(PKT_TYPE);
+
+    /*  Adding the parameterized sequence to the registery  */
+    typedef peg_l2_pkt_seq#(PKT_TYPE,SEQR_TYPE) this_type;
+    typedef ovm_object_registry#(this_type)type_id;
+
+    /*  Linking with p_sequencer  */
+    `ovm_declare_p_sequencer(SEQR_TYPE)
+
+
+    /*  Constructor */
+    function new(string name  = "peg_l2_pkt_seq");
+      super.new(name);
+    endfunction
+
+    /*  Body of sequence  */
+    task  body();
+      PKT_TYPE  pkt;
+
+      p_sequencer.ovm_report_info(get_name(),"Start of peg_l2_pkt_seq",OVM_LOW);
+
+      $cast(pkt,create_item(PKT_TYPE::get_type(),m_sequencer,$psprintf("l2_pkt")));
+
+      start_item(pkt);  //start_item has wait_for_grant()
+
+      pkt.l2_daddr  = 'h1;
+      pkt.l2_saddr  = 'h2;
+
+      p_sequencer.ovm_report_info(get_name(),$psprintf("Generated pkt - \n%s", pkt.sprint()),OVM_LOW);
+
+      finish_item(pkt);
+
+      #1;
+    endtask : body
+
+
+  endclass  : peg_l2_pkt_seq
+
+`endif
+
+/*
+ --------------------------------------------------------------------------
+
+ -- <Header>
+ 
+
+ -- <Log>
+
+[31-01-2016  04:27:46 PM][mammenx] Initial Commit
+
+[28-05-14 20:18:21] [mammenx] Moved log section to bottom of file
+
+ --------------------------------------------------------------------------
+*/
---
+++ wxp/verif/tb/tests/rmii/peg_rmii_l2_test.sv
@@ -0,0 +1,102 @@
+/*
+ --------------------------------------------------------------------------
+   Synesthesia-Moksha - Copyright (C) 2012 Gregory Matthew James.
+
+   This file is part of Synesthesia-Moksha.
+
+   Synesthesia-Moksha is free; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   Synesthesia-Moksha is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+*/
+
+/*
+ --------------------------------------------------------------------------
+ -- Project Code      : pegasus
+ -- Test Name         : peg_rmii_l2_test
+ -- Author            : mammenx
+ -- Function          : Base test that instantiates the cortex env & makes
+                        connections from DUT to TB interfaces.
+ --------------------------------------------------------------------------
+*/
+
+
+class peg_rmii_l2_test extends peg_rmii_base_test;
+
+    `ovm_component_utils(peg_rmii_l2_test)
+
+    //Sequences
+    peg_l2_pkt_seq#(super.L2_PKT_TYPE,super.RMII_RX_SEQR_TYPE)   seq;
+
+    OVM_FILE  f;
+    ovm_table_printer printer;
+
+
+    /*  Constructor */
+    function new (string name="peg_rmii_l2_test", ovm_component parent=null);
+        super.new (name, parent);
+    endfunction : new 
+
+
+    /*  Build */
+    function  void  build();
+      super.build();
+
+      f = $fopen({"./logs/",get_full_name(),".log"});
+
+      set_report_default_file(f);
+      set_report_severity_action(OVM_INFO,  OVM_DISPLAY | OVM_LOG);
+      set_report_severity_action(OVM_WARNING, OVM_DISPLAY | OVM_LOG);
+      set_report_severity_action(OVM_ERROR,  OVM_COUNT | OVM_DISPLAY | OVM_LOG);
+      set_report_severity_action(OVM_FATAL,  OVM_EXIT | OVM_DISPLAY | OVM_LOG);
+
+
+      ovm_report_info(get_full_name(),"Start of build",OVM_LOW);
+
+      seq = peg_l2_pkt_seq#(super.L2_PKT_TYPE,super.RMII_RX_SEQR_TYPE)::type_id::create("l2_pkt_seq", this);
+
+      ovm_report_info(get_full_name(),"End of build",OVM_LOW);
+    endfunction : build
+
+
+
+    /*  Run */
+    virtual task run ();
+      ovm_report_info(get_full_name(),"Start of run",OVM_LOW);
+
+      seq.start(super.env.rmii_agent.rx.seqr);
+
+      #1000;
+
+      global_stop_request();
+
+      ovm_report_info(get_full_name(),"End of run",OVM_LOW);
+    endtask : run 
+
+
+endclass : peg_rmii_l2_test
+
+/*
+ --------------------------------------------------------------------------
+
+ -- <Header>
+ 
+
+ -- <Log>
+
+[31-01-2016  04:27:46 PM][mammenx] Initial Commit
+
+
+ --------------------------------------------------------------------------
+*/
+
+
